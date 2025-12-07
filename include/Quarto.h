07888bcaaf64e455aa8ad1387c97e129c0c5be16
Bernardo Leão <bernardoleao104@gmail.com>
2025-12-07 14:42:25 -0300

    Arquivo da classe Quarto atualizado para correcao do problema critico de armazenamento

--- include/Quarto.h
+++ include/Quarto.h
@@ -1,27 +1,25 @@
 #ifndef QUARTO_H
 #define QUARTO_H
 
-#include <string>
+#include "Definicoes.h"
 
 class Quarto {
 private:
     int numero;
     int capacidade;
     float valorDiaria;
-    std::string status;
+    char status[MAX_TELEFONE]; 
 
 public:
-    Quarto(int num, int cap, float valor) : 
-        numero(num), capacidade(cap), valorDiaria(valor), status("DESOCUPADO") {}
-    
-    Quarto() : numero(0), capacidade(0), valorDiaria(0.0), status("INDEFINIDO") {}
+    Quarto(int num, int cap, float val, const char* stat);
+    Quarto() : numero(0), capacidade(0), valorDiaria(0.0f) {}
     
     int obterNumero() const { return numero; }
     int obterCapacidade() const { return capacidade; }
     float obterValorDiaria() const { return valorDiaria; }
-    std::string obterStatus() const { return status; }
-
-    void definirStatus(const std::string& novoStatus) { status = novoStatus; }
+    const char* obterStatus() const { return status; }
+    
+    void definirStatus(const char* novoStatus);
 };
 
 #endif
