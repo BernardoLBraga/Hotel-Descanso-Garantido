88cdc29e38f3dd019c9ecf70239f6a2b3eb86b7b
Bernardo Leão <bernardoleao104@gmail.com>
2025-12-07 14:26:28 -0300

    Classe Cliente atualizada com novo atributo pontosFidelidade

--- include/Cliente.h
+++ include/Cliente.h
@@ -9,17 +9,21 @@ private:
     std::string nome;
     std::string endereco;
     std::string telefone;
+    int pontosFidelidade;
 
 public:
-    Cliente(int cod, std::string nom, std::string end, std::string tel) : 
-        codigo(cod), nome(nom), endereco(end), telefone(tel) {}
+    Cliente(int cod, std::string n, std::string end, std::string tel) : 
+        codigo(cod), nome(n), endereco(end), telefone(tel), pontosFidelidade(0) {}
     
-    Cliente() : codigo(0) {}
+    Cliente() : codigo(0), pontosFidelidade(0) {}
     
     int obterCodigo() const { return codigo; }
     std::string obterNome() const { return nome; }
     std::string obterEndereco() const { return endereco; }
     std::string obterTelefone() const { return telefone; }
+    
+    int obterPontosFidelidade() const { return pontosFidelidade; }
+    void adicionarPontos(int pontos) { pontosFidelidade += pontos; } // NOVO MÉTODO
 };
 
 #endif
