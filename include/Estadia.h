e10dab0c19c1d9a14f98dda8d3119bcae02be7ab
Bernardo Leão <bernardoleao104@gmail.com>
2025-12-07 14:42:40 -0300

    Arquivo da classe Estadia atualizado para correcao do problema critico de armazenamento

--- include/Estadia.h
+++ include/Estadia.h
@@ -1,30 +1,27 @@
 #ifndef ESTADIA_H
 #define ESTADIA_H
 
-#include <string>
+#include "Definicoes.h"
 
 class Estadia {
 private:
     int codigoEstadia;
     int codigoCliente;
     int numeroQuarto;
-    std::string dataEntrada;
-    std::string dataSaida;
+    char dataEntrada[MAX_DATA];
+    char dataSaida[MAX_DATA];
     int qtdDiarias;
 
 public:
-    Estadia(int codE, int codC, int numQ, std::string dE, std::string dS, int qtdD) : 
-        codigoEstadia(codE), codigoCliente(codC), numeroQuarto(numQ), 
-        dataEntrada(dE), dataSaida(dS), qtdDiarias(qtdD) {}
-    
+    Estadia(int codE, int codC, int numQ, const char* dE, const char* dS, int qtdD);
     Estadia() : codigoEstadia(0), codigoCliente(0), numeroQuarto(0), qtdDiarias(0) {}
     
     int obterCodigoEstadia() const { return codigoEstadia; }
     int obterCodigoCliente() const { return codigoCliente; }
     int obterNumeroQuarto() const { return numeroQuarto; }
     int obterQtdDiarias() const { return qtdDiarias; }
-    std::string obterDataEntrada() const { return dataEntrada; }
-    std::string obterDataSaida() const { return dataSaida; }
+    const char* obterDataEntrada() const { return dataEntrada; }
+    const char* obterDataSaida() const { return dataSaida; }
 };
 
 #endif
